b912b6b516da00a74c834f9cef9ede22ac4e392b
Eric Driggers <admalledd@gmail.com>
2012-12-25 19:08:24 -0800

    merged linux and windows configurations, linux side first (check windows later)

--- sp.py
+++ sp.py
@@ -1,42 +1,50 @@
 import sublime, sublime_plugin
 import subprocess as sp
 import os,sys
-import fs.osfs
 
 class subprocessCommand(sublime_plugin.TextCommand):
     def run(self, edit, exe,args=None ,fname=None, curdir=None):
         if fname == None:
             fname = self.view.file_name()
-        if curdir ==None:
-            curdir = os.path.dirname(fname)
+        runner(exe,fname,curdir,args)
 
-        #print exe,fname,args
-        #print sublime.packages_path()
-
-        cmd=['start',"cmd.exe",'/C',os.path.join(sublime.packages_path(),'User','sp.bat'),exe,fname]
-        if args is not None:
-            cmd.append(*args)
-        print sp.list2cmdline(cmd)
-        proc = sp.Popen(cmd,cwd=curdir,shell=True)
 class subprocess_buildCommand(sublime_plugin.WindowCommand):
-
-    def run(self, exe=None,args=None ,fname=None, curdir=None,clean_localdir=None):
+    def run(self, exe,args=None ,fname=None, curdir=None):
         if fname == None:
             fname = self.view.file_name()
-        if curdir ==None:
-            curdir = os.path.dirname(fname)
+        runner(exe,fname,curdir,args)
 
-        print exe,fname,curdir
-        if clean_localdir:
-            #clean the local directory that is in %APPDATA%, $HOME/.config/ ect... (leaving root dir)
-            if sys.platform == 'win32':
-                rmcmd=["start","cmd.exe",'/C',os.path.join(sublime.packages_path(),'User','sp.bat'),
-                        'C:\\cygwin\\bin\\rm','-rf',  os.path.expandvars('$APPDATA/revabotica/')]
-                proc = sp.Popen(rmcmd,shell=True)
-                import time
-                time.sleep(2.5)
-        cmd=['start',"cmd.exe",'/C',os.path.join(sublime.packages_path(),'User','sp.bat'),exe,fname]
-        if args is not None:
-            cmd.append(*args)
-        print sp.list2cmdline(cmd)
+def runner(exe,fname,curdir,args,clean_localdir=None,dat=None):
+    if curdir ==None:
+        curdir = os.path.dirname(fname)
+
+    if sys.platform == "win32":
+        cmd = [
+                'start',
+                "cmd.exe",
+                '/C',
+                os.path.join(sublime.packages_path(),'User','sp.bat'),
+                exe,
+                fname
+              ]
+
+    elif sys.platform == "linux2":
+        cmd = [
+                'gnome-terminal',
+                '-x',
+                os.path.join(sublime.packages_path(),'User','run_linux_program_sub'),
+                curdir+"/",
+                exe,
+                fname
+              ]
+
+    if args is not None:
+        cmd.append(*args)
+
+    print sp.list2cmdline(cmd)
+    sublime.status_message('running subprocess')
+
+    if sys.platform == "win32":
         proc = sp.Popen(cmd,cwd=curdir,shell=True)
+    elif sys.platform == 'linux2':
+        proc = sp.Popen(cmd, stdin=sp.PIPE, stdout=sp.PIPE)
