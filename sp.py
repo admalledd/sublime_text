c780dace686dfd272d92413f28f81ae27e3284f9
Eric Driggers <admalledd@gmail.com>
2015-01-15 12:58:06 -0800

    Fix windows and spaces issues...
    kill me, I am handling Quote parsing by hand. This is probably very broken

--- sp.py
+++ sp.py
@@ -3,29 +3,34 @@ import subprocess as sp
 import os,sys
 
 class subprocessCommand(sublime_plugin.TextCommand):
-    def run(self, edit, exe,args=None ,fname=None, curdir=None):
+    def run(self, edit, exe,exe_args=None,args=None ,fname=None, curdir=None):
         if fname == None:
             fname = self.view.file_name()
-        runner(exe,fname,curdir,args)
+        runner(exe,fname,curdir,exe_args,args)
 
 class subprocess_buildCommand(sublime_plugin.WindowCommand):
-    def run(self, exe,args=None ,fname=None, curdir=None):
+    def run(self, exe,exe_args=None,args=None ,fname=None, curdir=None):
         if fname == None:
             fname = self.view.file_name()
-        runner(exe,fname,curdir,args)
+        runner(exe,fname,curdir,exe_args,args)
 
-def runner(exe,fname,curdir,args,clean_localdir=None,dat=None):
+def runner(exe,fname,curdir,exe_args,args,clean_localdir=None,dat=None):
     if curdir ==None:
         curdir = os.path.dirname(fname)
 
     if sys.platform == "win32":
+        if exe_args is not None:
+            exe = exe + '" "'+'" "'.join(exe_args)
+
+
+        subcmd = '""'+os.path.join(sublime.packages_path(),'User','sp.bat')+'" "'+\
+                exe+'" "'+\
+                fname+'""'
         cmd = [
                 'start',
                 "cmd.exe",
                 '/C',
-                os.path.join(sublime.packages_path(),'User','sp.bat'),
-                exe,
-                fname
+                subcmd
               ]
 
     elif sys.platform in ("linux2","linux" ):
@@ -34,17 +39,21 @@ def runner(exe,fname,curdir,args,clean_localdir=None,dat=None):
                 '-x',
                 os.path.join(sublime.packages_path(),'User','run_linux_program_sub'),
                 curdir+"/",
-                exe,
-                fname
+                exe
               ]
+        if exe_args is not None:
+            cmd.appen(*exe_args)
+        cmd.append(fname)
+        if args is not None:
+            cmd.append(*args)
 
-    if args is not None:
-        cmd.append(*args)
     print(sys.platform)
-    print(sp.list2cmdline(cmd))
+    print(cmd)
+    print(" ".join(cmd))
     sublime.status_message('running subprocess')
 
     if sys.platform == "win32":
-        proc = sp.Popen(cmd,cwd=curdir,shell=True)
+        os.system(" ".join(cmd))
+        #proc = sp.Popen(cmd,cwd=curdir)
     elif sys.platform in ("linux2","linux" ):
         proc = sp.Popen(cmd, stdin=sp.PIPE, stdout=sp.PIPE)
